c1b5de0cb83876dfd58050599c217ce0ce6dbfb0
Naohiro Sakuma <nao.bear@gmail.com>
2014-12-20 02:13:07 +0900

    Setup DB

--- main.go
+++ main.go
@@ -2,13 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/naoina/genmai"
 	"github.com/zenazn/goji"
 	"github.com/zenazn/goji/web"
 )
 
+// define a table schema.
+type Passwords struct {
+	Id          int64  `db:"pk" column:"id"`
+	Description string `default:"me"`
+	body        string
+	UserName    string `db:"unique" size:"255"`
+	genmai.TimeStamp
+}
+
 func main() {
+	db, err := genmai.New(&genmai.SQLite3Dialect{}, "./password-picker.db")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if err := db.CreateTableIfNotExists(&Passwords{}); err != nil {
+		log.Fatalln(err)
+	}
+
 	goji.Get("/", Root)
 	goji.Serve()
 }
